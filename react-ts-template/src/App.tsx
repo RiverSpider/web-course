d97767d4ff53896234b6a46c257602ea786ebfb7
RiverSpider <raevskih8888@mail.ru>
2024-03-01 17:27:52 +0600

    add header

--- react-ts-template/src/App.tsx
+++ react-ts-template/src/App.tsx
@@ -1,6 +1,9 @@
 import type { RouteObject } from "react-router-dom";
 import { Outlet, Link, useRoutes, useParams } from "react-router-dom";
+import Header from "./components/Header";
 
 export default function App() {
-  
+    return (
+        <Header />
+    );
 }
