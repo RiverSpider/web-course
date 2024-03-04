cd3fd2a4bab68ad77026a8948702c9c58a11de49
RiverSpider <raevskih8888@mail.ru>
2024-03-04 23:14:27 +0600

    add cards

--- react-ts-template/src/App.tsx
+++ react-ts-template/src/App.tsx
@@ -2,9 +2,12 @@ import type { RouteObject } from "react-router-dom";
 import { Outlet, Link, useRoutes, useParams } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Wrap from "./components/Wrap/Wrap";
+import { characters } from "./assets/styles/characters.tsx";
+import { comics } from "./assets/styles/comics.tsx";
 
 export default function App() {
     return (
-        <><Header /><Footer /></>
+        <><><Header /></><Wrap data={characters} /><Footer /></>
     );
 }
