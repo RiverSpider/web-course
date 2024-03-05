0a289e0a338d60d9b7d5bd853290645acf057939
RiverSpider <raevskih8888@mail.ru>
2024-03-05 17:10:30 +0600

    add routes

--- react-ts-template/src/App.tsx
+++ react-ts-template/src/App.tsx
@@ -1,14 +1,20 @@
-import type { RouteObject } from "react-router-dom";
+import { Routes, type RouteObject, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Wrap from "./components/Wrap/Wrap";
 import { characters } from "./assets/styles/characters.tsx";
 import { comics } from "./assets/styles/comics.tsx";
-import SearchBar from "./components/Search/Search.tsx";
 import SearchForm from "./components/Search/Search.tsx";
 
 export default function App() {
     return (
-        <><><Header /></><SearchForm /><Wrap data={characters} /><Footer /></>
+        <>
+            <Header />
+            <Routes>
+                <Route path="/characters" element={<><SearchForm /><Wrap data={characters} /></>} />
+                <Route path="/comics" element={<><SearchForm /><Wrap data={comics} /></>} />
+            </Routes>
+            <Footer />
+        </>
     );
 }
