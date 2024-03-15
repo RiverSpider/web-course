bc2305028ee29a9f4c43f9b6cc59204dff256dc6
RiverSpider <raevskih8888@mail.ru>
2024-03-15 22:14:52 +0600

    fix header logo

--- react-ts-template/src/components/Header/Header.tsx
+++ react-ts-template/src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   const location = useLocation();
   return (
     <header className={classes.header}>
-      <img src="../../assets/images/marvel_logo.svg" alt="Marvel Logo" className={classes.logo} />
+      <div className={classes.logo}></div>
       <nav className={classes.navigation_container}>
         <Link to='/characters' className={location.pathname === '/characters' ? `${classes.navigation_link} ${classes.active}` : classes.navigation_link}>Characters</Link>
         <Link to='/comics' className={location.pathname === '/comics' ? `${classes.navigation_link} ${classes.active}` : classes.navigation_link}>Comics</Link>
