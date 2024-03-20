4fed0ad3ba24cf8d754ebc290ed7a922de96a3c4
RiverSpider <raevskih8888@mail.ru>
2024-03-20 16:03:45 +0600

    add toats for char and comic by id

--- react-ts-template/src/routes/characters/id.tsx
+++ react-ts-template/src/routes/characters/id.tsx
@@ -3,6 +3,9 @@ import InfoPage from "./../../components/InfoPage/InfoPage.tsx";
 import { Character } from "../../api/types/post.ts";
 import posts from "../../api/posts.ts";
 import { useParams } from "react-router-dom";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const CharacterInfo = () => {
   const { id } = useParams();
@@ -12,10 +15,12 @@ const CharacterInfo = () => {
   useEffect(() => {
     const fetchCharacter = async () => {
       try {
-        const data = await posts.getCharacter(parseInt(id));
+        const data = await posts.getCharacter(parseInt(id || '0'));
         setCharacter(data);
       } catch (error) {
         console.error('Error fetching character:', error);
+        toast.error("Failed to load character. Please try again.");
+        throw error;
       }
     };
 
--- react-ts-template/src/routes/comics/id.tsx
+++ react-ts-template/src/routes/comics/id.tsx
@@ -3,6 +3,8 @@ import InfoPage from "./../../components/InfoPage/InfoPage.tsx";
 import { useState, useEffect } from "react";
 import posts from "../../api/posts.ts";
 import { Comic } from "../../api/types/post.ts";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ComicInfo = () => {
   const { id } = useParams();
@@ -11,10 +13,12 @@ const ComicInfo = () => {
   useEffect(() => {
     const fetchComic = async () => {
       try {
-        const data = await posts.getComic(parseInt(id));
+        const data = await posts.getComic(parseInt(id || '0'));
         setComic(data);
       } catch (error) {
         console.error('Error fetching comic:', error);
+        toast.error("Failed to load comics. Please try again.");
+        throw error;
       }
     };
 
