09d6ac0f22611b4d26b34bad627ddcb488092984
RiverSpider <raevskih8888@mail.ru>
2024-05-10 17:31:01 +0600

    feat: add service worker

--- react-ts-template/src/main.tsx
+++ react-ts-template/src/main.tsx
@@ -5,6 +5,18 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from './App'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(err => {
+        console.error('Cannot register Service Worker:', err);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
