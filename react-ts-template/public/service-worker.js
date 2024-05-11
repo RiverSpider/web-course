059e95ec4ec736d06882bd1adaef0ab6cd6ddf0a
RiverSpider <raevskih8888@mail.ru>
2024-05-11 16:56:51 +0600

    feat: add notification relink

--- react-ts-template/public/service-worker.js
+++ react-ts-template/public/service-worker.js
@@ -9,3 +9,20 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   console.log('Fetching:', event.request.url);
 });
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(windowClients => {
+      for (let i = 0; i < windowClients.length; i++) {
+        let client = windowClients[i];
+        if (client.url === '/' && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
+  );
+});
